de5234df8b7002f2fb7d004f79a6611a7afeea99
ismailahmed10 <140971981+ismailahmed10@users.noreply.github.com>
2024-06-06 21:20:10 +0100

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,15 @@
+export const geoApiOptions = {
+	method: "GET",
+	headers: {
+	  "X-RapidAPI-Key": "a5c3596ccamshafaafaa56e4878cp13f8d8jsne3fb794e85d1",
+	  "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+	},
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "734e8467504005c3c35f2c5a464e9006";
+
+
+
+
